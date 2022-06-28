6c7b7563800eff48544303e5a8d8f36d80949934
Abby422 <abbyuri22@gmail.com>
2022-06-28 15:35:22 +0300

    Deploy to github pages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Search from './component/Search';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={window.location.pathname || ''}>
       <Routes>
         <Route path='/' element={<App />} >
           <Route index element={<Discover />}/>
